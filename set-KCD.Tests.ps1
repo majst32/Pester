79cfa102381a7e42d32be16dcfa4dd7354053984
Missy Januszko <majst32@do-not-reply.com>
2016-07-29 15:09:50 -0400

    First attempt

---
+++ set-KCD.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "set-KCD" {
+
+    It "only allows a string to be passed to the SPNs parameter" {
+    Mock Set-ADAccountControl {return $Null}
+    Mock Set-ADObject {return $Null}
+
+       { Set-KCD -ADObject Whatever -SPNs [pscustomobject]@{} } | Should Not Throw
+    }
+}
---
+++ set-KCD.ps1
@@ -0,0 +1,24 @@
+﻿function set-KCD {
+[cmdletbinding(SupportsShouldProcess=$True,ConfirmImpact='Medium')]
+
+param (  
+    [parameter(Mandatory=$True,ValueFromPipeline=$True)]
+    $ADObject,
+
+    [parameter(Mandatory=$True,ValueFromPipeline=$True)]
+    [string[]]$SPNs
+        )
+
+
+if ($ADObject.AccountNotDelegated -eq $True) {
+    $ADObject | Set-ADAccountControl -AccountNotDelegated $False -TrustedForDelegation $False -TrustedToAuthForDelegation $True
+    }
+else {
+    $ADObject | Set-ADAccountControl -TrustedForDelegation $False -TrustedToAuthForDelegation $true
+    }
+foreach ($SPN in $SPNs) {
+    $SPN= $SPN.toString()
+    set-adObject -Identity $ADObject.distinguishedName -Add @{"msds-AllowedtoDelegateTo" = $SPN}
+    write-verbose "Kerberos Constrained Delegation applied on $ADObject.sAMAccountName for $SPN."
+    }
+}
